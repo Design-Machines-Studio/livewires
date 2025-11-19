51cec44e40676d158c94e62b925d5ddd19a9a1b7
louderthan10 <trav@louderthanten.com>
2025-11-19 14:02:50 +0800

    Cleaning up base styles and variables

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // Vite entry point for CSS and JavaScript
 // This file imports the CSS so Vite can process it correctly
-import './css/main.css';
+import '../css/main.css';
 
 // HTML Includes via Web Components
 import './html-include.js';
