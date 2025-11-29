e071d9fd660cfcafa5347bff9aade8e5deb9429e
louderthan10 <trav@louderthanten.com>
2025-11-29 21:13:46 +0800

    Clean up utility classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 // Vite entry point for CSS and JavaScript
 // This file imports the CSS so Vite can process it correctly
 import '../css/main.css';
+import '../css/prototyping.css';
 
 // HTML Includes via Web Components
 import './html-include.js';
